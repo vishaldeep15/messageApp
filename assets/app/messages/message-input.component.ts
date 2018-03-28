dd0bdb20de14f965f7caf7231d075e389eac531b
Vishal Deep <visdhiman15@gmail.com>
2018-03-28 16:28:10 -0400

    Add template validation to input field

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 
 import { MessageService } from './message.service';
 import { Message } from './message.model';
+import { NgForm } from "@angular/forms";
 
 @Component( {
     selector: 'app-message-input',
@@ -9,8 +10,9 @@ import { Message } from './message.model';
 })
 export class MessageInputComponent {
     constructor( private messageService: MessageService) {}
-    onSave(value: string) {
-        const message = new Message(value, 'Vishal');
+    onSubmit(form: NgForm) {
+        const message = new Message(form.value.content, 'Vishal');
         this.messageService.addMessage(message);
+        form.resetForm();
     }
 }
